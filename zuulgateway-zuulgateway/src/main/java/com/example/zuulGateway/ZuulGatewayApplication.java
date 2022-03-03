1555d7cac086893741103444fac0c1b3627ec44f
PradeepKumar <pradeep.chakravorty1@gmail.com>
2022-03-03 17:35:40 +0530

    add hello

--- zuulgateway-zuulgateway/src/main/java/com/example/zuulGateway/ZuulGatewayApplication.java
+++ zuulgateway-zuulgateway/src/main/java/com/example/zuulGateway/ZuulGatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,6 +15,11 @@ public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String hello() {
+		return "Hello Zuul Gateway";
 	}
 
 }
